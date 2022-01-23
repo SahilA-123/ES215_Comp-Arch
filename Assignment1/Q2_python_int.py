ac30914cb1d2260709ef0909059f40fd4b2ef108
SahilA-123 <76252705+SahilA-123@users.noreply.github.com>
2022-01-24 00:32:58 +0530

    Update Q2_python_int.py

--- Assignment1/Q2_python_int.py
+++ Assignment1/Q2_python_int.py
@@ -1,6 +1,5 @@
 import timeit
 import random as rand
-
 # to make the code simpler(choosing 1,2,3,4,5)
 dictionary = { 1:32, 2:64, 3:128, 4:256, 5:512 }
 print("Choose an option:\n 1:32,2:64,3:128,4:256,5:512\n")
@@ -13,16 +12,20 @@ n = dictionary[choice]
 A = [[rand.randint(0,50) for i in range(n)] for j in range(n)]
 B = [[rand.randint(0,50) for t in range(n)] for l in range(n)]
 
-# resultant matrix
+# resultant matrix (empty)
 res = [[0 for m in range(n)] for p in range(n)]
 
+# start time 
 start = timeit.default_timer()
 
+# calculating the resultant matrix
 for i in range(n):
     for j in range(n):
         for k in range(n):
-            res[i][j] += A[i][k]*B[k][j]
+            res[i][j] = res[i][j] + A[i][k]*B[k][j]
 
-
+# end time 
 stop = timeit.default_timer()
+
+# execution time is the difference end and start time
 print("Time : ", stop-start)
